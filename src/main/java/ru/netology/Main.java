99108a4bfb542703e387797c45a756459aad391c
Vladimir Pavlyukov <yahusim@mail.ru>
2022-09-20 23:19:03 +0300

    reworked

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -3,14 +3,14 @@ package ru.netology;
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService creditPaymentService = new CreditPaymentService();
-        int[] period = {1,2,3};
+        int[] period = {1, 2, 3};
         long[] expectedAmounts = {87_911L, 46_140L, 32_262L};
-        for(int i = 0; i < 3; i++){
-            long actualAmount = creditPaymentService.calculate(1000000L, 9.99, i+1);
-            if(expectedAmounts[i] == actualAmount){
+        for (int i = 0; i < 3; i++) {
+            long actualAmount = creditPaymentService.calculate(1000000L, 9.99, period[i]);
+            if (expectedAmounts[i] == actualAmount) {
                 System.out.println("Ожидаемый ежемесячный платеж("
                         + expectedAmounts[i] + ") равен рассчитанному ежемесячному платежу(" + actualAmount + ")");
-            }else {
+            } else {
                 System.out.println("Ожидаемый ежемесячный платеж("
                         + expectedAmounts[i] + ") не равен рассчитанному ежемесячному платежу(" + actualAmount + ")");
             }
